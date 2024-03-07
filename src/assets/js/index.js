f7689c6528bcf87afaf0b6acc6cb772f932c240d
pasnon lucas <107144928+warzskynox12@users.noreply.github.com>
2024-03-07 10:21:49 +0100

    1.0.1

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -29,9 +29,7 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Lancemet du launcher", "author": "Delta_skynox12" },
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
